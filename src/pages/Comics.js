87478a663bf3b9a454b4adeda42965516b1d2127
dev-loops-per <robertodevstack@gmail.com>
2022-12-12 22:06:32 -0300

    add image and routes

---
+++ src/pages/Comics.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Comics = () => {
+  return (
+    <div>
+        <h1>Estamos en seccion de comics</h1>
+    </div>
+  )
+}
---
+++ src/pages/Series.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export const Series = () => {
+  return (
+    <div>
+        <h1>Estamos en series</h1>
+    </div>
+  )
+}
